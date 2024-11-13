43d890cbc5c2b3473b58c53bab5b5c1351fda620
Max Unanian <Zeptorqyiad99@gmail.com>
2024-11-14 01:23:11 +0500

    Finished with compilled Categories components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 
-function Categories(props) {
+function Categories({ items }) {
+   const [activeItem, setActiveItem] = React.useState(null)
+
+   const onSelectItem = (index) => {
+      setActiveItem(index)
+   }
+
    return (
       <div className="categories">
          <ul>
-            <li className="active">Все</li>
-            <li>Мясные</li>
-            <li>Вегетарианская</li>
-            <li>Гриль</li>
-            <li>Острые</li>
-            <li>Закрытые</li>
+            <li
+               onClick={() => onSelectItem(null)}
+               className={activeItem === null ? 'active' : ''}
+            >
+               Все
+            </li>
+            {items &&
+               items.map((name, index) => (
+                  <li
+                     className={activeItem === index ? 'active' : ''}
+                     onClick={() => onSelectItem(index)}
+                     key={`${name}_${index}`}
+                  >
+                     {name}
+                  </li>
+               ))}
          </ul>
       </div>
    )
