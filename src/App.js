15d200d881b5f89e8e6bdca8dde487be5a7131a4
Max Unanian <Zeptorqyiad99@gmail.com>
2024-11-29 01:56:52 +0500

    Working with PizzaBlocks

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { Header } from './components'
 import { Home, Cart } from './pages'
 
 function App() {
+   const [pizzas, setPizzas] = React.useState([])
+
+   React.useEffect(() => {
+      fetch('http://localhost:3000/db.json')
+         .then((resp) => resp.json())
+         .then((json) => {
+            setPizzas(json.pizzas)
+         })
+   }, [])
+
+   console.log()
+
    return (
       <div className="wrapper">
          <Header />
          <div className="content">
             <Routes>
-               <Route path="/" element={<Home />} />
+               <Route path="/" element={<Home items={pizzas} />} />
                <Route path="/cart" element={<Cart />} />
             </Routes>
          </div>
